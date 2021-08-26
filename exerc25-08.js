2d18d70a6b23dfa2c46ed28b0beb7ec6b9a964b3
Heberth Fonseca <haf_1005@hotmail.com>
2021-08-25 22:28:04 -0300

    Trabalho em Mesa Dia 25/08 Prog-Imperativa

--- exerc25-08.js
+++ exerc25-08.js
@@ -1,81 +1,18 @@
-var menu = 6
-var aumentar = true
-var diminuir = false
-var tempoUsuario = 0
+let pipoca = 10;
+let macarrao = 8;
+let carne = 15;
+let feijao = 12;
+let brigadeiro = 8;
 
-if ((menu < 1) || (menu >5)) {
-    console.log("Prato inexistente")
-}
-else if ((aumentar) && (menu > 0) && (menu < 6)) {
-    let prato = aumentarTempo(menu, tempoUsuario)
-    console.log(prato)
-}
-else if ((diminuir) && (menu > 0) && (menu < 6)) {
-    let prato = diminuirTempo(menu, tempoUsuario)
-    console.log(prato)
-}
-
-function aumentarTempo (menu, tempoUsuario) {
-    let tempoPadrao = 0
-    let tempoTotal = 0
-    switch (menu) {
-        case 1:
-            tempoPadrao = 10
-            tempoTotal = tempoPadrao + tempoUsuario
-            break;
-         case 2:
-            tempoPadrao = 8
-            tempoTotal = tempoPadrao + tempoUsuario
-            break;
-        case 3:
-            tempoPadrao = 15
-            tempoTotal = tempoPadrao + tempoUsuario
-            break;
-        case 4:
-            tempoPadrao = 12
-            tempoTotal = tempoPadrao + tempoUsuario
-            break;
-        case 5:
-            tempoPadrao = 8
-            tempoTotal = tempoPadrao + tempoUsuario
-            break;
+function microondas(alimento, tempo) {
+    if ((tempo >= alimento) && (tempo < (alimento * 2))) {
+        console.log("Prato pronto, bom apatite!");
+    } else if (tempo > (alimento * 2) && (tempo <= (alimento * 3))) {
+        console.log("A comida queimou");
+    } else if (tempo < alimento) {
+        console.log("Tempo Insuficiente");
+    } else {
+        console.log("Kabuumm!!");
     }
-    if (tempoTotal > (2 * tempoPadrao) && (tempoTotal < (3 * tempoPadrao))) {
-        return "A comida queimou"
-    }
-    else if (tempoTotal > (3 * tempoPadrao)) {
-        return "Kabumm"
-    }
-    return "Prato pronto, bom apetite"
 }
-
-function diminuirTempo (menu, tempoUsuario) {
-    let tempoPadrao = 0
-    let tempoTotal = 0
-    switch (menu) {
-        case 1:
-            tempoPadrao = 10
-            tempoTotal = tempoPadrao - tempoUsuario
-            break;
-         case 2:
-            tempoPadrao = 8
-            tempoTotal = tempoPadrao - tempoUsuario
-            break;
-        case 3:
-            tempoPadrao = 15
-            tempoTotal = tempoPadrao - tempoUsuario
-            break;
-        case 4:
-            tempoPadrao = 12
-            tempoTotal = tempoPadrao - tempoUsuario
-            break;
-        case 5:
-            tempoPadrao = 8
-            tempoTotal = tempoPadrao - tempoUsuario
-            break;
-    }
-    if (tempoTotal >= tempoPadrao) {
-        return "Prato pronto, bom apetite"
-    }
-    return "Tempo insuficiente"
-}
+microondas(feijao, 30);
