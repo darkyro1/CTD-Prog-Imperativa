d25feb6ac21302e3b593239195a60efd19bdc97c
Heberth Fonseca <haf_1005@hotmail.com>
2021-08-31 22:45:54 -0300

    update

---
+++ exerc31-08.js
@@ -0,0 +1,22 @@
+for (let volta = 10; volta >= 1; volta--) {
+    console.log('Volta número: ' + volta);
+}
+
+
+
+console.log("____________ Repetir como um papagaio ____________ ");
+for (let world = 1; world <= 5; world++) {
+    console.log(world + " - Olá mundo.");
+}
+
+console.log("____________ Contando números ímpares ____________ ");
+for (let umDez = 1; umDez <=10; umDez++){
+    if (umDez % 2 !==0)
+    console.log(umDez);
+}
+
+console.log("____________ Criando a tabuada ____________ ");
+for (let tabuada = 1; tabuada <= 10; tabuada++){
+    console.log("2 x "+ tabuada + " = "+ 2 * tabuada);
+}
+
--- test.js
+++ test.js
@@ -1,2 +1,40 @@
+function conversor(pol) {
+    return pol * 2.54
+}
+console.log('1 polegada é equivalente a ' + conversor(1) + 'cm');
 
-console.log(10 !== 10);
+
+
+
+
+function convert(valor) {
+    var valor = "funcaojs";
+    return valor = "http://www.funcaojs.com.br"
+}
+console.log(convert());
+
+
+
+
+function fun1(valore) {
+    var valore = 'papel de parede';
+    return '!' + valore;
+}
+console.log(fun1());
+
+
+
+function idadeDog(num) {
+    let idadeHumano = 7
+    return idadeHumano * num
+}   
+console.log(idadeDog(2));
+
+
+function horaHomem(){
+    let salario = 2000;
+    return salario / 160;
+}
+console.log(horaHomem());
+
+
